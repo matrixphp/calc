549b32e300a338db4bab3bb0c071c81036837af5
Anatolii <anatoliy.s@parnas-it.com>
2022-11-26 20:59:54 +0300

    Initial Version

---
+++ src/modules/main.ts
@@ -0,0 +1,49 @@
+import {Notify, QNotifyCreateOptions} from 'quasar';
+
+export type historyItem = {
+  data: string,
+  value: number
+}
+
+export function notify( message: string, icon = 'error', color = '#323232', textColor = 'white', timeout = 10000, position: QNotifyCreateOptions['position'] = 'bottom', multiLine = false) {
+  console.log(message)
+  Notify.create({
+    message,
+    icon,
+    color,
+    textColor,
+    multiLine,
+    timeout,
+    position,
+    actions: [{
+      label: '', color: textColor, icon: 'close', handler: () => { /***/
+      }
+    }]
+  })
+}
+
+export function setLS<T>(name: string, data: T): T {
+  localStorage[name] = JSON.stringify(data);
+  return data;
+}
+
+export function getLS<T>(name: string, data_default: T | string = ''): T | string {
+  if (localStorage[name] === undefined) return data_default;
+  return JSON.parse(localStorage[name]);
+}
+
+export function themeColors (dark = false) {
+  return{
+    calc: dark? 'grey-2' : 'primary',
+    hist: dark? 'grey-5' : 'grey-7',
+    input: dark? 'grey-5' : 'primary',
+    btn1: dark? {bg: 'grey-9', txt: 'white'} : {bg: 'grey-1', txt: 'black'},
+    btn2: dark? {bg: 'grey-6', txt: 'black'} : {bg: 'grey-3', txt: 'black'},
+    btn3: dark? {bg: 'orange-14', txt: 'white'} : {bg: 'orange-8', txt: 'white'},
+    btn4: dark? {bg: 'grey-7', txt: 'white'} : {bg: 'primary', txt: 'white'},
+    toggle: dark? 'orange' : 'grey',
+    histDel: dark? 'grey-5' : 'grey-9',
+    histItem: dark? 'blue-grey' : 'grey',
+    histHead: dark? 'blue-grey' : 'grey',
+  }
+}
---
+++ src/pages/IndexPage.vue
@@ -0,0 +1,176 @@
+<template>
+  <q-page class="row justify-evenly">
+    <div class="row q-mt-sm">
+      <div class="row items-start">
+        <div style="display: grid; grid-template-columns: repeat(4, 1fr); grid-gap: 5px">
+          <q-btn dense class="button-size" :color="theme.btn2.bg" :text-color="theme.btn2.txt" @click="dataAdd('(')">
+            <template v-slot:default>
+              <div style="font-size: 1.1rem">(</div>
+            </template>
+          </q-btn>
+          <q-btn dense class="button-size" :color="theme.btn2.bg" :text-color="theme.btn2.txt" @click="dataAdd(')')">
+            <template v-slot:default>
+              <div style="font-size: 1.1rem">)</div>
+            </template>
+          </q-btn>
+          <q-btn dense class="button-size" :color="theme.btn2.bg" :text-color="theme.btn2.txt" @click="dataAdd('.')">
+            <template v-slot:default>
+              <div style="font-size: 1.3rem">.</div>
+            </template>
+          </q-btn>
+          <q-btn dense class="button-size" :color="theme.btn2.bg" :text-color="theme.btn2.txt" @click="dataPercent()">
+            <template v-slot:default>
+              <div style="font-size: 1.1rem">%</div>
+            </template>
+          </q-btn>
+
+          <q-btn dense class="button-size" :color="theme.btn1.bg" :text-color="theme.btn1.txt" label="7" @click="dataAdd('7')"/>
+          <q-btn dense class="button-size" :color="theme.btn1.bg" :text-color="theme.btn1.txt" label="8" @click="dataAdd('8')"/>
+          <q-btn dense class="button-size" :color="theme.btn1.bg" :text-color="theme.btn1.txt" label="9" @click="dataAdd('9')"/>
+          <q-btn dense class="button-size" :color="theme.btn3.bg" :text-color="theme.btn3.txt" @click="dataAdd('/')">
+            <template v-slot:default>
+              <div style="font-size: 1.1rem">/</div>
+            </template>
+          </q-btn>
+
+          <q-btn dense class="button-size" :color="theme.btn1.bg" :text-color="theme.btn1.txt" label="4" @click="dataAdd('4')"/>
+          <q-btn dense class="button-size" :color="theme.btn1.bg" :text-color="theme.btn1.txt" label="5" @click="dataAdd('5')"/>
+          <q-btn dense class="button-size" :color="theme.btn1.bg" :text-color="theme.btn1.txt" label="6" @click="dataAdd('6')"/>
+          <q-btn dense class="button-size" :color="theme.btn3.bg" :text-color="theme.btn3.txt" icon="close" size="sm" @click="dataAdd('*')"/>
+
+          <q-btn dense class="button-size" :color="theme.btn1.bg" :text-color="theme.btn1.txt" label="1" @click="dataAdd('1')"/>
+          <q-btn dense class="button-size" :color="theme.btn1.bg" :text-color="theme.btn1.txt" label="2" @click="dataAdd('2')"/>
+          <q-btn dense class="button-size" :color="theme.btn1.bg" :text-color="theme.btn1.txt" label="3" @click="dataAdd('3')"/>
+          <q-btn dense class="button-size" :color="theme.btn3.bg" :text-color="theme.btn3.txt" icon="remove" size="sm" @click="dataAdd('-')"/>
+
+          <q-btn dense class="button-size" :color="theme.btn1.bg" :text-color="theme.btn1.txt" label="0" @click="dataAdd(0)"/>
+          <q-btn dense class="button-size" :color="theme.btn4.bg" :text-color="theme.btn4.txt" style="grid-column: 2/4; width: 6.4rem;" @click="dataCalculate()">
+            <template v-slot:default>
+              <div style="font-size: 1.3rem">=</div>
+            </template>
+          </q-btn>
+          <q-btn dense class="button-size" :color="theme.btn3.bg" :text-color="theme.btn3.txt" icon="add" size="sm" @click="dataAdd('+')"/>
+
+          <q-btn flat round :color="theme.toggle" icon="brightness_7" size="0.8rem" @click="themeToggle(!dark)"/>
+        </div>
+      </div>
+      <div class="q-ml-sm">
+        <div class="row">
+          <q-input dense outlined clearable autofocus v-model="data" :color="theme.input" class="no-wrap expression" style="width: 500px;" @keydown.enter.prevent="dataCalculate()">
+            <template v-slot:prepend>
+              <q-icon name="calculate" :color="theme.calc"/>
+            </template>
+          </q-input>
+        </div>
+        <div class="q-mt-sm">
+          <q-list bordered class="rounded-borders" style="max-width: 500px;">
+            <q-item v-ripple>
+              <q-item-section>
+                <q-item-label header style="padding: 0">
+                  <q-icon :icon="theme.histHead" :color="theme.hist" name="history" size="1.4rem" class="q-mr-sm"></q-icon>
+                  История
+                </q-item-label>
+              </q-item-section>
+
+              <q-item-section side>
+                <q-btn flat round icon="close" :color="theme.histDel" size="0.7rem" @click="historyClear"/>
+              </q-item-section>
+            </q-item>
+            <q-separator/>
+
+            <template v-for="(v, k) in history" :key="v">
+              <q-item>
+                <q-item-section>
+                  <q-item-label caption overline class="q-ml-sm" style="font-size: 1rem; cursor: pointer;" @click="dataAdd(v.data)">{{ v.data }}</q-item-label>
+                  <q-item-label class="q-ml-lg" style="font-size: 1.1rem; cursor: pointer;" @click="dataAdd(v.result)">= {{ v.result }}</q-item-label>
+                </q-item-section>
+
+                <q-item-section side>
+                  <q-btn flat round icon="delete" :color="theme.histDel" size="0.6rem" style="margin-right: 5px" @click="historyDel(v)"/>
+                </q-item-section>
+              </q-item>
+              <q-separator inset v-if="k !== history.length - 1"/>
+            </template>
+
+            <q-item v-if="!history.length">
+              <q-item-section>
+                <q-item-label caption>
+                  <div class="q-ml-md">—</div>
+                </q-item-label>
+              </q-item-section>
+            </q-item>
+          </q-list>
+        </div>
+      </div>
+    </div>
+  </q-page>
+</template>
+
+<script setup lang="ts">
+import {ref} from 'vue';
+import {useQuasar} from 'quasar';
+import {getLS, historyItem, notify, setLS, themeColors} from 'src/modules/main';
+
+const quasar = useQuasar();
+const data = ref<string>('');
+const dark = ref<boolean>(getLS('dark', false) as boolean);
+const theme = ref({});
+const history = ref(getLS('history', []) as object[]);
+
+themeToggle(dark.value);
+
+function themeToggle(v: boolean) {
+  setLS('dark', v);
+  dark.value = v;
+  theme.value = themeColors(v);
+  quasar.dark.set(v);
+}
+
+function dataAdd(v) {
+  if (data.value === null) data.value = '';
+  data.value += v;
+}
+
+function dataPercent() {
+  data.value = parseFloat(data.value) * 0.01 + '';
+}
+
+function dataCalculate() {
+  try {
+    if(data.value === '') return;
+
+    data.value = data.value.replaceAll(',', '.');
+    data.value = data.value.replaceAll(' ', '');
+    let result = eval(data.value);
+    history.value.push({data: data.value, result: result})
+    data.value = '';
+    setLS('history', history.value);
+  }
+  catch (e) {
+    notify('Введено некорректное значение');
+  }
+}
+
+function historyDel(item: historyItem) {
+  history.value = history.value.filter(v => v !== item);
+  setLS('history', history.value);
+}
+
+function historyClear() {
+  history.value = [];
+  setLS('history', history.value);
+}
+</script>
+
+<style lang="scss">
+.button-size {
+  height: 3rem;
+  width: 3rem;
+}
+
+.expression {
+  .q-field__control, .q-field__marginal {
+    height: 3rem;
+  }
+}
+</style>
